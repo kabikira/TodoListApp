05407f43ca9b0e01bdba717d5bc07607d0852ae6
kabikira <kabikirar2@protonmail.com>
2023-08-06 18:21:16 +0900

    add NetworkMonitorクラス実装

--- TodoListApp/Utility/NetworkMonitor.swift
+++ TodoListApp/Utility/NetworkMonitor.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Network
+
+
+final class NetworkMonitor {
+    static let shared = NetworkMonitor()
+    private let monitor: NWPathMonitor
+    static let connectionLost = Notification.Name("Not connected to the network.")
+
+    private init() {
+        self.monitor = NWPathMonitor()
+    }
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+            } else {
+                NotificationCenter.default.post(name: NetworkMonitor.connectionLost, object: nil)
+            }
+        }
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
