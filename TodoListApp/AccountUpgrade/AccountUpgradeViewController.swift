80876b57c18694f6bfda93dbb634ca2894c3750a
kabikira <kabikirar2@protonmail.com>
2023-08-08 23:21:28 +0900

    add AccountUpgradeViewController,AccountUpgradeストーリーボード追加

---
+++ TodoListApp/AccountUpgrade/AccountUpgradeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AccountUpgradeViewController.swift
+//  TodoListApp
+//
+//  Created by koala panda on 2023/08/08.
+//
+
+import UIKit
+
+class AccountUpgradeViewController: UIViewController {
+
+    @IBOutlet private weak var AccountUpgradeLabel: UILabel!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var done: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
